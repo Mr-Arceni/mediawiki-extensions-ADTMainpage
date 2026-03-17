6fa2524358212f54fdff77b2bfab6a5c2d4c2894
Mr. Arceni <nikitin.arceny@gmail.com>
2026-03-17 22:36:07 +0100

    add ApiServerStatus.php

---
+++ src/includes/ApiServerStatus.php
@@ -0,0 +1,29 @@
+<?php
+namespace MediaWiki\Extension\ADTMainpage;
+
+use ApiBase;
+use MediaWiki\Http\HttpRequestFactory;
+
+class ApiServerStatus extends ApiBase {
+    private $httpRequestFactory;
+    private const $address = '193.164.18.155:1212';
+
+    public function __construct( $main, $action, HttpRequestFactory $httpRequestFactory ) {
+        parent::__construct( $main, $action );
+        $this->httpRequestFactory = $httpRequestFactory;
+    }
+
+    public function execute() {
+        $url = 'http://'.$address.'/status';
+
+        $request = $this->httpRequestFactory->get( $url, [ 'timeout' => 3 ] );
+
+        if ( $request ) {
+            $this->getResult()->addValue( null, 'status_data', json_decode( $request ) );
+        } else {
+            $this->dieWithError( 'Server unavailable' );
+        }
+    }
+
+    public function getAllowedParams() { return []; }
+}
