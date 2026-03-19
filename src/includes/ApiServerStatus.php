cb697a81342eb1f6b1bfb6f143254c0216a66427
Mr. Arceni <nikitin.arceny@gmail.com>
2026-03-19 15:04:24 +0100

    fix syntax error

--- src/includes/ApiServerStatus.php
+++ src/includes/ApiServerStatus.php
@@ -7,6 +7,7 @@ use MediaWiki\Http\HttpRequestFactory;
 class ApiServerStatus extends ApiBase {
     private $httpRequestFactory;
     private const $address = '193.164.18.155:1212';
+    private const ADDRESS = '193.164.18.155:1212';
 
     public function __construct( $main, $action, HttpRequestFactory $httpRequestFactory ) {
         parent::__construct( $main, $action );
@@ -14,7 +15,7 @@ class ApiServerStatus extends ApiBase {
     }
 
     public function execute() {
-        $url = 'http://'.$address.'/status';
+        $url = 'http://'.self::ADDRESS.'/status';
 
         $request = $this->httpRequestFactory->get( $url, [ 'timeout' => 3 ] );
 
